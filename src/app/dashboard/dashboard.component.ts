0632d0b584426c44ca3f9fd3fe0ece3fb464f52f
MarcoHilliges <107026106+MarcoHilliges@users.noreply.github.com>
2022-08-04 15:00:59 +0200

    user finished

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { collection, collectionData, Firestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  allUser:any = [];
+
+  users$!:Observable<any>;
+
+  constructor(private firestore: Firestore) { }
 
   ngOnInit(): void {
+    const coll = collection(this.firestore, 'Users');
+    this.users$ = collectionData(coll);
+
+    this.users$.subscribe((usersFromServer:any)=>{
+      // console.log(usersFromServer);
+      this.allUser = usersFromServer;
+      console.log(this.allUser);
+      
+    })
   }
 
 }
